4fc45b1872c30a8057f899ae3c5db001bf32a8ca
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-20 20:38:55 +0900

    implement signout function in the client page;

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,23 @@
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
   const { data: session } = authClient.useSession();
+
+  const signout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+          toast.success("Signed out successfully");
+        },
+      },
+    });
+  };
 
   return (
     <div>
@@ -17,6 +31,8 @@ export default function Home() {
         <>
           <p>{session.user.name}</p>
           <p>{session.user.email}</p>
+
+          <Button onClick={signout}>Logout</Button>
         </>
       ) : (
         <Button>Login</Button>
