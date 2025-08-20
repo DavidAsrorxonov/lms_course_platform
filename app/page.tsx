7f5e718748970d7a638ac3ee7d04ab387e71491d
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-20 17:01:16 +0900

    create themetoggle.jsx; wrap the app with theme provider; implement login ui;

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { ThemeToggle } from "@/components/ui/themeToggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold bg-blue-500">Hello World</h1>
+
+      <ThemeToggle />
     </div>
   );
 }
