12d5df3fe023f41b3a509fa481dfcc9defe7e6b0
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-20 17:42:30 +0900

    import and use logos;

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,56 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeftIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+
+import LogoLight from "../../public/SB_1_light.png";
+import LogoDark from "../../public/SB_1_dark.png";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
-      <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
+      <Link
+        href={"/"}
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute top-4 left-4",
+        })}
+      >
+        <ArrowLeftIcon className="size-4" />
+        Back
+      </Link>
+
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href={"/"}
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <Image
+            src={LogoLight}
+            alt="logo"
+            width={200}
+            height={200}
+            className="dark:hidden"
+            priority
+          />
+          <Image
+            src={LogoDark}
+            alt="logo"
+            width={200}
+            height={200}
+            className="hidden dark:block"
+            priority
+          />
+        </Link>
+
+        {children}
+
+        <div className="text-balance text-center text-xs text-muted-foreground">
+          By clicking continue, you agree to our <span>Terms of Service</span>{" "}
+          and <span>Privacy Policy</span>.
+        </div>
+      </div>
     </div>
   );
 };
