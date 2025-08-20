7bba9aa43c72a1cc88011420239bdb9a7ccb359e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-20 12:24:08 +0900

    configure shadcn ui library;

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Hello World</h1>
+      <h1 className="text-3xl font-bold bg-blue-500">Hello World</h1>
     </div>
   );
 }
