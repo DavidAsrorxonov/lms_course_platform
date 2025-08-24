e5b07087a14af19a9ee198f9197f81b09156f9a0
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-24 14:23:24 +0900

    add more proctection;

--- app/admin/courses/create/actions.ts
+++ app/admin/courses/create/actions.ts
@@ -1,18 +1,52 @@
 "use server";
 
-import { auth } from "@/lib/auth";
+import { requireAdmin } from "@/app/data/admin/require-admin";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
 import { prisma } from "@/lib/db";
 import { ApiResponse } from "@/lib/types";
 import { courseSchema, CourseSchemaInput } from "@/lib/zodSchemas";
-import { headers } from "next/headers";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5,
+    })
+  );
 
 export const CreateCourse = async (
   data: CourseSchemaInput
 ): Promise<ApiResponse> => {
+  const session = await requireAdmin();
+
   try {
-    const session = await auth.api.getSession({
-      headers: await headers(),
+    const req = await request();
+    const decision = await aj.protect(req, {
+      fingerprint: session.user.id as string,
     });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return {
+          status: "error",
+          message: "You have been blocked due to rate limiting",
+        };
+      } else {
+        return {
+          status: "error",
+          message:
+            "Looks like you are a bot. If this is a mistake, please contact support.",
+        };
+      }
+    }
 
     const validation = courseSchema.safeParse(data);
 
