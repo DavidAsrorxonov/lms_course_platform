940baaefdd7e338bd4f58d738e2465be8d7be848
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-24 15:13:10 +0900

    add dropdown menu;

--- app/admin/courses/_components/AdminCourseCard.tsx
+++ app/admin/courses/_components/AdminCourseCard.tsx
@@ -1,8 +1,23 @@
 import { AdminCourseType } from "@/app/data/admin/admin-get-courses";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { useConstructUrl } from "@/hooks/use-construct-url";
-import { ArrowRight, SchoolIcon, TimerIcon } from "lucide-react";
+import {
+  ArrowRight,
+  EyeIcon,
+  MoreVertical,
+  PencilIcon,
+  SchoolIcon,
+  TimerIcon,
+  Trash2,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -15,7 +30,39 @@ export const AdminCourseCard = ({ data }: iAppProps) => {
 
   return (
     <Card className="group relative py-0 gap-0">
-      <div></div>
+      <div className="absolute top-2 right-2 z-10">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"secondary"} size={"icon"}>
+              <MoreVertical className="size-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-48">
+            <DropdownMenuItem asChild>
+              <Link href={`/admin/courses/${data.id}/edit`}>
+                <PencilIcon className="size-4 mr-2" />
+                Edit Course
+              </Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem asChild>
+              <Link href={`/admin/courses/${data.slug}`}>
+                <EyeIcon className="size-4 mr-2" />
+                Preview
+              </Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem asChild>
+              <Link href={`/admin/courses/${data.id}/delete`}>
+                <Trash2 className="size-4 mr-2 text-destructive" />
+                Delete Course
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
 
       <Image
         src={thumbnailUrl}
