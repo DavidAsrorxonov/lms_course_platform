e3ea3cd7f5e1872036848528227192301acfda7f
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-24 15:07:50 +0900

    create and atyle admin card;

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -16,7 +16,7 @@ const CoursesPage = async () => {
         </Link>
       </div>
 
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
         {data.map((course) => (
           <AdminCourseCard data={course} key={course.id} />
         ))}
