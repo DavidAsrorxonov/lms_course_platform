5b1675f55914d9f7f105d3e23bfa32c4f330455e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-27 12:31:40 +0900

    work on public facing side of the app;

---
+++ app/data/course/get-course.ts
@@ -0,0 +1,43 @@
+import { prisma } from "@/lib/db";
+import { notFound } from "next/navigation";
+
+export const getOneCourse = async (slug: string) => {
+  const course = await prisma.course.findUnique({
+    where: {
+      slug: slug,
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      price: true,
+      duration: true,
+      level: true,
+      fileKey: true,
+      category: true,
+      smallDescription: true,
+      chapter: {
+        select: {
+          id: true,
+          title: true,
+          lessons: {
+            select: {
+              id: true,
+              title: true,
+            },
+            orderBy: {
+              position: "asc",
+            },
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) return notFound();
+
+  return course;
+};
