7a1c9791c7ed826d47ddc9c6190af78a7dec6e97
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-27 17:57:15 +0900

    create a user validation function;

---
+++ app/(public)/courses/[slug]/actions.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import { requireUser } from "@/app/data/user/require-user";
+import { prisma } from "@/lib/db";
+import { ApiResponse } from "@/lib/types";
+
+export const enrollInCourseAction = async (courseId: string) => {
+  const user = await requireUser();
+
+  try {
+    const course = await prisma.course.findUnique({
+      where: {
+        id: courseId,
+      },
+      select: {
+        id: true,
+        title: true,
+        price: true,
+        slug: true,
+      },
+    });
+
+    if (!course) {
+      return {
+        status: "error",
+        message: "Course not found",
+      };
+    }
+
+    let stripeCustomerId: string;
+    const userWithStripeCustomerId = await prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+      select: {
+        stripeCustomerId: true,
+      },
+    });
+
+    if (userWithStripeCustomerId?.stripeCustomerId) {
+      stripeCustomerId = userWithStripeCustomerId.stripeCustomerId;
+    } else {
+    }
+  } catch (error) {
+    return {
+      status: "error",
+      message: "Failed to enroll in course",
+    };
+  }
+};
