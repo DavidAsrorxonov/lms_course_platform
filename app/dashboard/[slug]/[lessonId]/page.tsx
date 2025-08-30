c339fb9b82d58951a75d9e7876379f93bd987ba2
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-30 23:12:35 +0900

    PROJECT FINISHED!

--- app/dashboard/[slug]/[lessonId]/page.tsx
+++ app/dashboard/[slug]/[lessonId]/page.tsx
@@ -1,15 +1,24 @@
 import { getLessonContent } from "@/app/data/course/get-lesson-content";
-import React from "react";
+import React, { Suspense } from "react";
 import CourseContent from "./_components/CourseContent";
+import LessonSkeleton from "./_components/LessonSkeleton";
 
 type Params = Promise<{ lessonId: string }>;
 
 const LessonContentPage = async ({ params }: { params: Params }) => {
   const { lessonId } = await params;
 
-  const data = await getLessonContent(lessonId);
-
-  return <CourseContent data={data} />;
+  return (
+    <Suspense fallback={<LessonSkeleton />}>
+      <LessonContentLoader lessonId={lessonId} />;
+    </Suspense>
+  );
 };
 
 export default LessonContentPage;
+
+async function LessonContentLoader({ lessonId }: { lessonId: string }) {
+  const data = await getLessonContent(lessonId);
+
+  return <CourseContent data={data} />;
+}
