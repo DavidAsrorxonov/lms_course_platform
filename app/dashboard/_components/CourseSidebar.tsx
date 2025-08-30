7ea92e06150e1a5ba230a1362c763e69ac220ac7
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-30 15:36:00 +0900

    work on the UI;

--- app/dashboard/_components/CourseSidebar.tsx
+++ app/dashboard/_components/CourseSidebar.tsx
@@ -1,6 +1,14 @@
 import { CourseSidebarDataType } from "@/app/data/course/get-course-sidebar-data";
-import { Play } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { Progress } from "@/components/ui/progress";
+import { ChevronDown, Play } from "lucide-react";
 import React from "react";
+import LessonItem from "./LessonItem";
 
 interface iAppProps {
   course: CourseSidebarDataType["course"];
@@ -16,10 +24,59 @@ const CourseSidebar = ({ course }: iAppProps) => {
           </div>
 
           <div className="flex-1 min-w-0">
-            <h1>{course.title}</h1>
-            <p>{course.category}</p>
+            <h1 className="font-semibold text-base leading-tight truncate">
+              {course.title}
+            </h1>
+            <p className="text-sm text-muted-foreground m-1 truncate">
+              {course.category}
+            </p>
           </div>
         </div>
+
+        <div className="space-y-2">
+          <div className="flex justify-between text-xs">
+            <span className="text-muted-foreground">Progress</span>
+            <span className="font-medium">4/10 Lessons</span>
+          </div>
+
+          <Progress value={55} className="h-1.5" />
+          <p className="text-xs text-muted-foreground">55% Complete</p>
+        </div>
+      </div>
+
+      <div className="py-4 pr-4 space-y-3">
+        {course.chapter.map((chapter, index) => (
+          <Collapsible key={chapter.id} defaultOpen={index === 0}>
+            <CollapsibleTrigger asChild>
+              <Button
+                variant={"outline"}
+                className="w-full p-3 h-auto flex items-center gap-2"
+              >
+                <div className="shrink-0">
+                  <ChevronDown className="size-4 text-primary" />
+                </div>
+                <div className="flex-1 text-left min-w-0">
+                  <p className="font-semibold text-sm truncate text-foreground">
+                    {chapter.position}: {chapter.title}
+                  </p>
+
+                  <p className="text-[10px] text-muted-foreground font-medium truncate">
+                    {chapter.lessons.length} Lessons
+                  </p>
+                </div>
+              </Button>
+            </CollapsibleTrigger>
+            <CollapsibleContent className="mt-3 pl-6 border-l-2 space-y-3">
+              {chapter.lessons.map((lesson) => (
+                <LessonItem
+                  key={lesson.id}
+                  lesson={lesson}
+                  slug={course.slug}
+                />
+              ))}
+            </CollapsibleContent>
+          </Collapsible>
+        ))}
       </div>
     </div>
   );
---
+++ app/dashboard/_components/LessonItem.tsx
@@ -0,0 +1,59 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Check, Play } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+interface iAppProps {
+  lesson: {
+    id: string;
+    title: string;
+    position: number;
+    description: string | null;
+  };
+  slug: string;
+}
+
+const LessonItem = ({ lesson, slug }: iAppProps) => {
+  const completed: boolean = true;
+
+  return (
+    <Link
+      href={"/"}
+      className={buttonVariants({
+        variant: completed ? "secondary" : "outline",
+        className: cn(
+          "w-full p-2.5 h-auto justify-start transition-all",
+          completed &&
+            "bg-green-100 dark:bg-green-900/30 border-green-300 dark:border-green-700 hover:bg-green-200 dark:hover:bg-green-900/50 text-green-800 dark:text-green-200"
+        ),
+      })}
+    >
+      <div className="flex items-center gap-2.5 w-full min-w-0">
+        <div className="shrink-0">
+          {completed ? (
+            <div className="size-5 rounded-full bg-green-600 dark:bg-green-500 flex justify-center items-center">
+              <Check className="size-3 text-white" />
+            </div>
+          ) : (
+            <div
+              className={cn(
+                "size-5 rounded-full border-2 bg-background flex justify-center items-center"
+              )}
+            >
+              <Play className={cn("size-2.5 fill-current")} />
+            </div>
+          )}
+        </div>
+
+        <div>
+          <p className={cn("text-xs font-medium truncate")}>
+            {lesson.position}. {lesson.title}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default LessonItem;
